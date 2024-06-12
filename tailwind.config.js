028532b5e06e2ff2efef85d0deef401760cdf1c4
jsha64 <jorgemerino743@gmail.com>
2024-06-12 00:28:14 -0500

    Initial commit from Vite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-import type { Config } from "tailwindcss";
-
-const config: Config = {
+/** @type {import('tailwindcss').Config} */
+export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,8 +12,16 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "core-bg": "#1E1F22",
+        "core-secBg": "#282A2E",
+        "core-thBg": "#383B42",
+        "core-text": "#EEEEEE",
+        "core-secText": "#ABABAB",
+        "core-primary": "#10A37F",
+        "core-bbBg": "#4B4F5B",
+      },
     },
   },
   plugins: [],
 };
-export default config;
