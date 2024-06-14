f1af746106560b467ecfa69f2bca3dcfc82e9acb
jsha64 <jorgemerino743@gmail.com>
2024-06-13 21:24:35 -0500

    Initial commit from Create Next App

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-export default {
+import type { Config } from "tailwindcss";
+
+const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -12,16 +13,8 @@ export default {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
-      colors: {
-        "core-bg": "#1E1F22",
-        "core-secBg": "#282A2E",
-        "core-thBg": "#383B42",
-        "core-text": "#EEEEEE",
-        "core-secText": "#ABABAB",
-        "core-primary": "#10A37F",
-        "core-bbBg": "#4B4F5B",
-      },
     },
   },
   plugins: [],
 };
+export default config;
